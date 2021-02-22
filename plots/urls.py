8bf3574fa81db3e4463b46bf1f8883a69c4f062e
danielmedbar <danielmedbar@gmail.com>
2021-02-22 10:56:37 +0000

    fixes

--- plots/urls.py
+++ plots/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.plot, name='plots'),
+  path('', views.create_plot, name='plots'),
 ]
--- plots/views.py
+++ plots/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def plot(request):
+def create_plot(request):
   return HttpResponse("Hello, world. This is the site for plots.")
 
